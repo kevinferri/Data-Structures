8d564580922884fa1fef9b1043828bc7fa165e60
Kevin Ferri <kevinferri@gmail.com>
2014-06-16 10:40:23 -0400

    Update MyQuickSort.java

--- src/prep/MyQuickSort.java
+++ src/prep/MyQuickSort.java
@@ -3,13 +3,8 @@ package prep;
 import java.util.ArrayList;
 
 public class MyQuickSort {
-
-    /**
-     * @param args
-     */
+    
     public static void main(String[] args) {
-
-        //int[] a = { 1, 23, 45, 2, 8, 134, 9, 4, 2000 };
         int a[]={23,44,1,2009,2,88,123,7,999,1040,88};
         quickSort(a, 0, a.length - 1);
         System.out.println(a);
