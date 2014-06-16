cd3bbfd499a994335884658e9bd200674415fe74
Kevin Ferri <kevinferri@gmail.com>
2014-06-16 10:38:45 -0400

    Update ListNode.java

--- src/prep/ListNode.java
+++ src/prep/ListNode.java
@@ -69,8 +69,6 @@ public class ListNode {
 		return true;
 	}
 	
-	
-	/* MAIN */
 	public static void main(String[] args) {
 		ListNode l1 = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
 		l1.insertBefore(100);
