1beade347e90c850fcf8808da7e247743385d995
Kevin Ferri <kevinferri@gmail.com>
2014-12-25 00:38:45 -0500

    Update MyQuickSort.java

--- src/prep/MyQuickSort.java
+++ src/prep/MyQuickSort.java
@@ -5,17 +5,17 @@ import java.util.ArrayList;
 public class MyQuickSort {
 
     public static void main(String[] args) {
-        int a[]={23,44,1,2009,2,88,123,7,999,1040,88};
+        int a[]={23, 44, 1, 2009, 2, 88, 123, 7, 999, 1040, 88};
         quickSort(a, 0, a.length - 1);
         System.out.println(a);
         ArrayList al = new ArrayList();
     }
 
     public static void quickSort(int[] a, int p, int r) {
-        if(p<r) {
-            int q=partition(a,p,r);
-            quickSort(a,p,q);
-            quickSort(a,q+1,r);
+        if(p < r) {
+            int q=partition(a, p, r);
+            quickSort(a, p, q);
+            quickSort(a, q + 1, r);
         }
     }
 
@@ -27,9 +27,9 @@ public class MyQuickSort {
 
         while (true) {
             i++;
-            while ( i< r && a[i] < x)
+            while (i < r && a[i] < x)
                 i++;
-            j--;
+                j--;
             while (j>p && a[j] > x) {
                 j--;
             }
